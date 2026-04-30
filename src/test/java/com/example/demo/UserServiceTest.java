45cf91e3f0fd3af3ff5ff94b2439119ccb66400c
BZN-SAGAR-SINGH <sagar.s@byzan.com>
2026-04-30 12:03:58 +0530

    Fix test DB to H2

--- src/test/java/com/example/demo/UserServiceTest.java
+++ src/test/java/com/example/demo/UserServiceTest.java
@@ -5,12 +5,12 @@ import com.example.demo.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceTest {
-
     @Autowired
     UserService service;
 
@@ -18,7 +18,7 @@ class UserServiceTest {
     void testSaveUser() {
         User user = new User();
         user.setName("CI Test");
-        user.setEmail("ci@test.com");
+        user.setEmail("sagar.s@byzan.com");
 
         User saved = service.save(user);
 
